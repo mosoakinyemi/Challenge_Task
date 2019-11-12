0220270661a4d20cc635ca3925d360cb99bcf8d0
Akinyemi Mosolasi <akinyemimosolasi@gmail.com>
2019-11-12 20:18:47 +0100

    Added Icons ttf file

--- src/screens/Index.js
+++ src/screens/Index.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { View, Image, Dimensions, TouchableOpacity, Text } from 'react-native'
-import { Icon} from '../../../App.js'
+import Icon from '../components/Icon'
 import EStyleSheet from 'react-native-extended-stylesheet'
 import { createBottomTabNavigator, BottomTabBar } from 'react-navigation-tabs'
-import { store } from '../../redux/reducer'
-import { connect } from 'react-redux'
 
- const Dashboard = createBottomTabNavigator(
+export const Index = createAppContainer(AppRootNavigation)
+
+const AppRootNavigation = createStackNavigator({
+  Dashboard: { screen: Dashboard, navigationOptions: { header: null } },
+  AddCard: { screen: AddCard }
+})
+
+const Dashboard = createBottomTabNavigator(
   {
     Home: Home,
     Statement: Statement,
+    Add: Add,
     CashOut: CashOut,
     Profile: Profile
   },
@@ -17,79 +23,76 @@ import { connect } from 'react-redux'
     defaultNavigationOptions: ({ navigation }) => ({
       tabBarIcon: ({ focused, horizontal, tintColor }) => {
         const { routeName } = navigation.state
-        // You can return any component that you like here!
         return <TabIcon tintColor={tintColor} routeName={routeName} />
       }
     }),
     tabBarOptions: {
+      showLabel: false,
       activeTintColor: '#ffa500',
       inactiveTintColor: '#ddd',
-      labelStyle: {
-        fontSize: 15
-      },
       style: {
-        // backgroundColor: 'limegreen',
         height: 90,
-        paddingBottom: 20
+        // paddingBottom: 20
       }
     }
   }
 )
 
-class TabIconComponent extends React.PureComponent {
-  render () {
-    var { routeName, tintColor } = this.props
-    var badgeCount = this.props.ordersBadgeCount
+const TabIcon = (props) =>{
+    const { routeName, tintColor } = props
+
     let iconName
+
     if (routeName === 'Dashboard') {
       iconName = 'home'
-    } else if (routeName === 'Orders') {
-      iconName = 'users'
-    } else if (routeName === 'Chefs') {
-      iconName = 'chef'
-    } else if (routeName === 'Users') {
-      iconName = 'users'
+    } else if (routeName === 'Statement') {
+      iconName = 'file'
+    } else if (routeName === 'Add') {
+        iconName = 'plus'
+    } else if (routeName === 'CashOut') {
+      iconName = 'naira'
+    } else if (routeName === 'Profile') {
+      iconName = 'profile'
     }
-    return (
-      <View>
-        <MyIcon
-          name={iconName}
-          size={EStyleSheet.value('30rem')}
-          color={tintColor}
-        />
-        {badgeCount !== 0 && routeName === 'Orders' && (
-          <View style={styles.badgeContainer}>
-            <Text1 style={styles.badgeCount}>{badgeCount}</Text1>
-          </View>
-        )}
-      </View>
-    )
+
+    if(routeName==='Add'){
+        return (
+            <View style={styles.addIconContainer}>
+              <Icon
+                name={iconName}
+                size={EStyleSheet.value('20rem')}
+                color="white"
+              />
+            </View>
+          )
+    }
+    else{
+        return (
+            <View style={tyles.iconContainer}>
+              <Icon
+                name={iconName}
+                size={EStyleSheet.value('20rem')}
+                color={tintColor}
+              />
+              <Text style={styles.label}>{routeName}</Text>
+            </View>
+          )
+    }
   }
-}
 
 
 const styles = EStyleSheet.create({
-  badgeContainer: {
-    width: '20rem',
-    height: '20rem',
-    borderRadius: '10rem',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '$green',
-    position: 'absolute',
-    top: -7,
-    right: -7
-  },
-  badgeCount: {
-    color: '#fff',
-    fontSize: '12rem'
-  }
+    addIconContainer:{
+       height:'30rem',
+       width:'30rem',
+       borderRadius:'7rem',
+       backgroundColor:'$green'
+    },
+    label:{
+        fontSize:'15rem'
+    },
+    iconContainer:{
+        alignItems:'center',
+
+    }
 })
-
-
-const AppRootNavigation =  createStackNavigator({
-  OrderSuccess: { screen: OrderSuccess},
-  UserTabs:{screen:UserTabs,navigationOptions: { header: null }},
-});
-
-export default UserNav = createAppContainer(AppRootNavigation);
